36dec190d4bb4ae3617d5b4ff4b57bc5fae44919
WavJaby <WavJaby@gmail.com>
2023-11-09 13:02:06 +0800

    Update rwd

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 $(function () {
-
-  $("#navbarNav a").on('click', function (event) {
-
-    if (this.hash !== "") {
-
-      event.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').stop().animate({ scrollTop: $(hash).offset().top }, 200, function () {
-
-        window.location.hash = hash;
-      });
-    }
-  });
+	$("#navbarNav a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').stop().animate({scrollTop: $(hash).offset().top}, 200, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
 
 });
 
@@ -21,34 +15,34 @@ $(function () {
 // Services
 const nextBtn = document.querySelector(".serviceContainer .next-btn");
 const previousBtn = document.querySelector(".serviceContainer .previous-btn");
-const serviceContent =document.querySelector(".serviceContainer .content" );
+const serviceContent = document.querySelector(".serviceContainer .content");
 
 // press left right icons can scroll
-nextBtn.addEventListener("click", ()=>{
-  serviceContent.scrollLeft+=300;
+nextBtn.addEventListener("click", () => {
+	serviceContent.scrollLeft += 300;
 })
 
-previousBtn.addEventListener("click", ()=>{
-  serviceContent.scrollLeft-=300;
+previousBtn.addEventListener("click", () => {
+	serviceContent.scrollLeft -= 300;
 })
 
 //hide botton when we reach the end of each sides
-serviceContent.addEventListener("scroll", ()=>{
-  if(serviceContent.scrollLeft<=24){
-    previousBtn.classList.remove("active");
-  }else{
-    previousBtn.classList.add("active");
-  }
+serviceContent.addEventListener("scroll", () => {
+	if (serviceContent.scrollLeft <= 24) {
+		previousBtn.classList.remove("active");
+	} else {
+		previousBtn.classList.add("active");
+	}
 
-  console.log(serviceContent.scrollWidth)
-  console.log(serviceContent.clientWidth)
+	console.log(serviceContent.scrollWidth)
+	console.log(serviceContent.clientWidth)
 
 
-  let maxScrollValue= serviceContent.scrollWidth - serviceContent.clientWidth-24;
+	let maxScrollValue = serviceContent.scrollWidth - serviceContent.clientWidth - 24;
 
-  if(serviceContent.scrollLeft>=maxScrollValue){
-    nextBtn.classList.remove("active");
-  }else{
-    nextBtn.classList.add("active");
-  }
+	if (serviceContent.scrollLeft >= maxScrollValue) {
+		nextBtn.classList.remove("active");
+	} else {
+		nextBtn.classList.add("active");
+	}
 });
